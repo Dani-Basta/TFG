3dfd1cb794ad56b4df2ac1abbe433b78e388325c
javiberd <33233141+javiberd@users.noreply.github.com>
2017-11-07 00:25:52 +0100

    Add files via upload

--- knn_next.R
+++ knn_next.R
@@ -1,14 +1,15 @@
 #' Predict next value of the time series using k-nearest neighbors algorithm
 #'
-#' @param y A time series
+#' @param x A time series
 #' @param k Number of neighbors
 #' @param d Length of the "neighbourhoods"
 #' @param v Variable to be predicted if given multivariate time series
 #' @param metric Type of metric to evaluate the distance between points
 #' @return The predicted value
 
-knn_next = function(y, k, d, v=1, metric="euclidean"){
+knn_next = function(x, k, d, v=1, metric="euclidean"){
   require(rdist)
+  y <- matrix(x, ncol = NCOL(x))
   n <- NROW(y)
   m <- NCOL(y)
   roof <- n - d
@@ -17,19 +18,12 @@ knn_next = function(y, k, d, v=1, metric="euclidean"){
   neighs <- knn_neighs(y, d)
 
   # Calculamos las distancias omitiendo las columnas que tiene los valores reales
-  distances <- cdist(neighs[1:roof, 1:(d * m)], matrix(neighs[roof + 1, 1:(d * m)], nrow = 1), ...)
+  distances <- cdist(neighs[1:roof, 1:(d * m)], matrix(neighs[roof + 1, 1:(d * m)], nrow = 1))
   
   # Buscamos las k distancias mas pequenas y luego los indices correspondientes
   k_nn <- head( which( distances %in% head(sort.int(distances), k) ), k )
   
-  if (any(class(y) == "mts")) {
-    #prediction <- mean(y[(k_nn + d), v])
-    prediction <- weighted.mean(y[(k_nn + d), v], 1:k)
-    
-  } else {
-    #prediction <- mean(y[(k_nn + d)])
-    prediction <- weighted.mean(y[(k_nn + d)], 1:k)
-  }
+  prediction <- weighted.mean(y[(k_nn + d), v], k:1)
   
   ts(prediction)
 }
--- knn_past.R
+++ knn_past.R
@@ -1,45 +1,41 @@
 #' Predict next value of the time series using k-nearest neighbors algorithm
 #'
-#' @param y A time series
+#' @param x A time series
 #' @param k Number of neighbors
 #' @param d Length of the "neighbourhoods"
 #' @param v Variable to be predicted if given multivariate time series
 #' @return The predicted value
 
-knn_past = function(y, k, d, init, v=1, metric="euclidean"){
+knn_past = function(x, k, d, init, v=1, metric="euclidean"){
   require(rdist)
+  y <- matrix(x, ncol = NCOL(x))
   n <- NROW(y)
   m <- NCOL(y)
   roof <- n - d
   
+  # Obtenemos la matriz de vecindarios
   neighs <- knn_neighs(y, d)
   
+  # Reservamos array para los valores a predecir
   prediction <- array(dim = n - init)
   
-  distances <- rdist(neighs[, 1:(d * m)], ...)
+  # Calculamos las distancias (todos contra todos)
+  raw_distances <- rdist(neighs[, 1:(d * m)])
+  
+  # Transformamos la 'matriz triangular' a una matriz normal triangular inferior
+  distances <- diag(n - 1)
+  distances[lower.tri(distances, diag=TRUE)] <- raw_distances
   
   for (j in init:roof) {
     
-    # Generamos los indices que nos interesan para acceder a la fila correspondiente de la matriz triangular inferior de distancias
-    sums <- 0
-    for (i in 2:j) {
-      sums[i] <- sums[i - 1] - (i - 1)
-    }
-    
-    indexes <- sums + (0:(j - 1)) * roof + j
-    
-    dists <- distances[indexes]
+    # Obtenemos la fila de distancias que nos interesa para predicir el instante j+1
+    distances_rowj <- distances[j, 1:j]
     
     # Buscamos las k distancias mas pequenas y luego los indices correspondientes
-    k_nn <- head(which( dists %in% head(sort.int(dists, k)), k))
-    #print(k_nn)
-    if (any(class(y) == "mts")) {
-      prediction[j - init] <- mean(y[(k_nn + d), v])
-    } else {
-      prediction[j - init] <- mean(y[(k_nn + d)])
-    }
+    k_nn <- head(which( distances_rowj %in% head(sort.int(distances_rowj, k)), k))
+    
+    prediction[j - init + 1] <- mean(y[(k_nn + d), v])
   }
   
   ts(prediction)
-  
 }
