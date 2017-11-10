0cc58f3198f72b9e6318224730710de48c7fb2ca
javiberd <33233141+javiberd@users.noreply.github.com>
2017-11-10 15:52:09 +0100

    Add files via upload

--- knn_next.R
+++ knn_next.R
@@ -1,50 +1,37 @@
-#' Predict next value of the time series using k-nearest neighbors algorithm
+#' Predicts next value of the time series using k-nearest neighbors algorithm
 #'
 #' @param x A time series
 #' @param k Number of neighbors
-#' @param d Length of the "neighbourhoods"
+#' @param d Length of the 'neighbourhoods'
 #' @param v Variable to be predicted if given multivariate time series
 #' @param metric Type of metric to evaluate the distance between points
 #' @param weight Type of weight to use at the time of the prediction. 3 supported: proximity, same, trend 
 #' @return The predicted value
 
-knn_next = function(x, k, d, v=1, metric="euclidean", weight="proximity"){
-  require(rdist)
-  y <- matrix(x, ncol = NCOL(x))
-  n <- NROW(y)
-  m <- NCOL(y)
-  roof <- n - d
+knn_next = function(x, k, d, v = 1, metric = "euclidean", weight = "proximity") {
+    require(rdist)
+    y <- matrix(x, ncol = NCOL(x))
+    n <- NROW(y)
+    m <- NCOL(y)
+    roof <- n - d
+    
+    # Get 'neighbourhoods' matrix
+    neighs <- knn_neighs(y, d)
+    
+    # Calculate distances between the last 'neighbourhood' and each of rest 'neighbourhoods'
+    distances <- cdist(neighs[1:roof, 1:(d * m)], matrix(neighs[roof + 1, 1:(d * m)], nrow = 1))
+    
+    # Get the indexes of the k nearest neighbors
+    k_nn <- head((sort.int(distances, index.return = TRUE))$ix, k)
+    
+    # Calculate the weights for the future computation of the weighted mean 
+    # ------------Falta tratamiento correcto del valor delta------------------
+    weights = switch(weight, proximity = {1/(distances[k_nn] + 0.00001)}, 
+                             same = {rep.int(1, k)}, 
+                             trend = {k:1})
   
-  # Obtenemos la matriz de vecindarios
-  neighs <- knn_neighs(y, d)
-
-  # Calculamos las distancias omitiendo las columnas que tiene los valores reales
-  distances <- cdist(neighs[1:roof, 1:(d * m)], matrix(neighs[roof + 1, 1:(d * m)], nrow = 1) )
-  
-  # Buscamos las k distancias mas pequenas y luego los indices correspondientes
-  #k_nn <- head( which( distances %in% head(sort.int(distances), k) ), k )
-  # Esto: (sort.int(distances, index.return = TRUE))$ix da los indices antiguos de los elementos ordenados
-  k_nn <- head((sort.int(distances, index.return = TRUE))$ix, k)
-  
-  # prediction <- weighted.mean(y[(k_nn + d), v], k:1)
-  
-  # Hay que acceder a la TS 1 única vez  al principio, a partir de ahí se usa la matriz de vecindarios
-  # prediction <- weighted.mean(neighs[k_nn, k*d + v], k:1)
-  
-  # Hacemos la predicción sobre la matriz de vecindarios y ponderamos según el tipo elegido
-  weights =  switch(weight,
-        proximity = {
-            # Falta mirar casos de 1/0
-            1/distances[k_nn]
-        },
-        same = {
-            rep.int(1,k)
-        },
-        trend = {
-            k:1
-        }
-    )
-  prediction <- weighted.mean(neighs[k_nn, k*d + v], weights)
-  
-  prediction
+    # Calculate the predicted value
+    prediction <- weighted.mean(neighs[k_nn, k * d + v], weights)
+    
+    prediction
 }
--- knn_past.R
+++ knn_past.R
@@ -1,62 +1,50 @@
-#' Predict values of the time series using k-nearest neighbors algorithm
+#' Predicts values of the time series using k-nearest neighbors algorithm
 #'
 #' @param x A time series
 #' @param k Number of neighbors
-#' @param d Length of the "neighbourhoods"
-#' @param init 
+#' @param d Length of the 'neighbourhoods'
+#' @param init A value that has to satisfy d <= init < n, values are predicted from this point of the time series to the end
 #' @param v Variable to be predicted if given multivariate time series
 #' @param metric Type of metric to evaluate the distance between points
 #' @param weight Type of weight to use at the time of the prediction. 3 supported: proximity, same, trend 
 #' @return The predicted value
 
-knn_past = function(x, k, d, init, v=1, metric="euclidean", weight="proximity"){
-  require(rdist)
-  y <- matrix(x, ncol = NCOL(x))
-  n <- NROW(y)
-  m <- NCOL(y)
-  roof <- n - d
-  
-  # Obtenemos la matriz de vecindarios
-  neighs <- knn_neighs(y, d)
-  
-  # Reservamos array para los valores a predecir
-  prediction <- array(dim = n - init)
-  
-  # Calculamos las distancias (todos contra todos)
-  raw_distances <- rdist(neighs[, 1:(d * m)])
-  
-  # Transformamos la 'matriz triangular' a una matriz normal triangular inferior
-  distances <- diag(n - 1)
-  distances[lower.tri(distances, diag = FALSE)] <- raw_distances
-  # Se deja la primera fila de la matriz vacia, para aportar legibilidad a la hora de acceder
-  
-  for (j in init:roof) {
+knn_past = function(x, k, d, init, v = 1, metric = "euclidean", weight = "proximity") {
+    require(rdist)
+    y <- matrix(x, ncol = NCOL(x))
+    n <- NROW(y)
+    m <- NCOL(y)
+    roof <- n - d
+    prediction <- array(dim = n - init)
     
-    # Obtenemos la fila de distancias que nos interesa para predicir el instante j+1 suponiendo que 
-    # tenemos toda la informacion de 1 hasta j y desconocemos el futuro
-    distances_rowj <- distances[j, 1:(j - 1)]
+    # Get 'neighbourhoods' matrix
+    neighs <- knn_neighs(y, d)
+
+    # Calculate distances between every 'neighbourhood', a 'triangular matrix' is returned
+    raw_distances <- rdist(neighs[, 1:(d * m)])
     
-    # Buscamos las k distancias mas pequenas y luego los indices correspondientes
-    #k_nn <- head(which( distances_rowj %in% head(sort.int(distances_rowj, k)), k))
-    k_nn <- head((sort.int(distances_rowj, index.return = TRUE))$ix, k)
+    # Transform previous 'triangular matrix' in a regular matrix
+    distances <- diag(n - 1)
+    distances[lower.tri(distances, diag = FALSE)] <- raw_distances
     
-    # prediction[j - init + 1] <- weighted.mean(y[(k_nn + d), v], k:1)
+    for (j in init:roof) {
+        
+        # Get row needed from the distances matrix in order to predict instant j+1 asumming that all we
+        # know about the time series is instants 1 to j
+        distances_rowj <- distances[j, 1:(j - 1)]
+        
+        # Get the indexes of the k nearest neighbors
+        k_nn <- head((sort.int(distances_rowj, index.return = TRUE))$ix, k)
+        
+        # Calculate the weights for the future computation of the weighted mean 
+        # ------------Falta tratamiento correcto del valor delta------------------
+        weights = switch(weight, proximity = {1/(distances[k_nn] + 0.00001)}, 
+                         same = {rep.int(1, k)}, 
+                         trend = {k:1})
+        
+        # Calculate the predicted value
+        prediction[j - init + 1] <- weighted.mean(neighs[k_nn, k * d + v], weights)
+    }
     
-    # Hacemos la predicción sobre la matriz de vecindarios y ponderamos según el tipo elegido
-    weights =  switch(weight,
-                      proximity = {
-                          # Falta mirar casos de 1/0
-                          1/distances[k_nn]
-                      },
-                      same = {
-                          rep.int(1,k)
-                      },
-                      trend = {
-                          k:1
-                      }
-        )
-    prediction[j - init + 1] <- weighted.mean(neighs[k_nn, k*d + v], weights)
-  }
-  
-  ts(prediction)
+    ts(prediction)
 }
