4c7d9e4616834b2fc276a26a3b36848c06df2d32
javiberd <33233141+javiberd@users.noreply.github.com>
2017-11-10 14:56:17 +0100

    Add files via upload
    Comentarios a ingles

--- knn_neighs.R
+++ knn_neighs.R
@@ -1,28 +1,29 @@
-#' Create a matrix to be used for calculating distances and errors
+#' Creates a matrix to be used for calculating distances and errors
 #'
 #' @param y A matrix
-#' @param d Length of the "neighbourhoods"
+#' @param d Length of the 'neighbourhoods'
 #' @return A matrix to be used for calculating distances and errors
 
 knn_neighs = function(y, d) {
-  n <- NROW(y)
-  m <- NCOL(y)
-  roof <- n - d
-
-  # Pre-reservando la matriz
-  neighs <- matrix(nrow = roof + 1, ncol = d * m + m)
-  col <- 1
-  for (i in 1:m) {
-    for (j in 1:d) {
-      neighs[, col] <- y[(j:(j + roof)), i]
-      col <- col + 1
+    n <- NROW(y)
+    m <- NCOL(y)
+    roof <- n - d
+    
+    # Fill matrix in a way in which every row has a 'neighbourhood'
+    neighs <- matrix(nrow = roof + 1, ncol = d * m + m)
+    col <- 1
+    for (i in 1:m) {
+        for (j in 1:d) {
+            neighs[, col] <- y[(j:(j + roof)), i]
+            col <- col + 1
+        }
     }
-  }
-  
-  # Rellenado de las columnas de valores reales
-  for (i in 1:m) {
-    neighs[1:roof, d * m + i] <- y[(d + 1):n, i]
-  }
-  
-  neighs
+    
+    # Fill the last column of the matrix assigning to each neighbourhood the instant value 
+    # next to most recent instant value of the 'neighbourhood'
+    for (i in 1:m) {
+        neighs[1:roof, d * m + i] <- y[(d + 1):n, i]
+    }
+    
+    neighs
 }
