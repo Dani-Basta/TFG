4f2067286d41332587074667a946669d5dca388b
javiberd <33233141+javiberd@users.noreply.github.com>
2018-03-02 05:29:09 +0100

    Documentation added and unused variables deleted

--- knn_distances.R
+++ knn_distances.R
@@ -1,26 +1,28 @@
-knn_distances = function(x, d, distance_metric="euclidean", threads = 3, file){
+#' Calculate one distances matrix per each D for the given time series
+#' 
+#' @param x A time series
+#' @param d Values of Ds to be analyzed
+#' @param distance_metric Type of metric to evaluate the distance between points. Many metrics are supported: euclidean, manhattan, 
+#' dynamic time warping, camberra and others. For more information about the supported metrics check the values that 'method' 
+#' argument of function parDist (from parallelDist package) can take as this is the function used to calculate the distances. 
+#' Link to the package info: https://cran.r-project.org/web/packages/parallelDist
+#' Some of the values that this argument can take are "euclidean", "manhattan", "dtw", "camberra", "chord".
+#' @param threads Number of threads to be used when parallelizing
+#' @param file Name or id of the files where the distances matrixes will be saved
+knn_distances = function(x, d, distance_metric = "euclidean", threads = 3, file){
     require(parallelDist)
-    
-    # Calculate all d values to be explored. If a number is given, it creates a vector from 1 to k.
-    # Otherwise it will just make sure that the vector is ordered
-    if (length(d) == 1) {
-        d <- 1:d
-    } else if (is.unsorted(d)) {
-        d <- sort(d)
-    }
     
     # Initialization of variables to be used
     y <- matrix(x, ncol = NCOL(x))
     n <- NROW(y)
     
-    # In order to paralelise we calculate the distances matrix just once for each d, as the distance variates 
-    # with the number of values that characterise each element.
+    # In order to parallelize we calculate the distances matrix just once for each d, as the distance variates 
+    # with the number of values that characterize each element
     
-    #Calculate all distances matrixes
-    j <- 1
-    for (i in d) {
+    #Calculate and save all distances matrixes
+    for (act_d in d) {
         # Get elements matrix
-        elements_matrix <- knn_elements(y, i)
+        elements_matrix <- knn_elements(y, act_d)
         
         # Calculate distances between the last 'element' and each of the others 'elements'
         # This happens if d=1 and a univariate time series is given, a very unusual case
@@ -30,13 +32,11 @@ knn_distances = function(x, d, distance_metric="euclidean", threads = 3, file){
         raw_distances <- parDist(elements_matrix, distance_metric, threads = threads)
         
         # Transform previous 'triangular matrix' in a regular matrix
-        distances_new_element <- diag(n - i + 1)
+        distances_new_element <- diag(n - act_d + 1)
         distances_new_element[lower.tri(distances_new_element, diag = FALSE)] <- raw_distances
         
         # Save distances matrix in file
-        saveRDS(distances_new_element, paste0(file, i))
-        
-        j <- j + 1
+        saveRDS(distances_new_element, paste0(file, act_d))
     }
     
 }
