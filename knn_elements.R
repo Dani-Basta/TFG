426182307ac0d41b5cc0ec4f9b9db419c7417788
javiberd <33233141+javiberd@users.noreply.github.com>
2018-03-10 01:30:57 +0100

    Format change and new documentation added

--- knn_elements.R
+++ knn_elements.R
@@ -1,23 +1,25 @@
-#' Create a matrix to be used for calculating distances
+#' Create a matrix to be used for calculating distances. The most
+#' recent 'element' is put in the first row of the matrix, the
+#' second most recent 'element' in the second row and so on.
 #'
 #' @param y A matrix
-#' @param d Length of each of the elements
+#' @param d Length of each of the 'elements'
 #' @return A matrix to be used for calculating distances
 
 knn_elements = function(y, d) {
-    n <- NROW(y)
-    m <- NCOL(y)
-    last_elem <- n - d
-    
-    # Fill matrix in a way in which every row has an 'element'
-    elements_matrix <- matrix(nrow = last_elem + 1, ncol = d * m)
-    col <- 1
-    for (i in 1:m) {
-        for (j in 1:d) {
-            elements_matrix[, col] <- y[(j:(j + last_elem)), i]
-            col <- col + 1
-        }
+  n <- NROW(y)
+  m <- NCOL(y)
+  last_elem <- n - d
+
+  # Fill matrix in a way in which every row has an 'element'
+  elements_matrix <- matrix(nrow = last_elem + 1, ncol = d * m)
+  col <- 1
+  for (i in 1:m) {
+    for (j in 1:d) {
+      elements_matrix[, col] <- rev(y[(j:(j + last_elem)), i])
+      col <- col + 1
     }
-    
-    elements_matrix
+  }
+
+  elements_matrix
 }
