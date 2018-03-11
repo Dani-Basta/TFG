7493a71ad511532960b9b2b8d58f60994310fb0f
javiberd <33233141+javiberd@users.noreply.github.com>
2018-03-11 04:59:35 +0100

    More efficient and elegant way of accesing distance column and reading file

--- knn_optim_parallelf2.R
+++ knn_optim_parallelf2.R
@@ -1,138 +1,115 @@
 #' Optimize the values of K and D for a given time series
-#' 
+#'
 #' @param x A time series
 #' @param k Values of Ks to be analyzed
 #' @param d Values of Ds to be analyzed
 #' @param v Variable to be predicted
 #' @param error_metric Type of metric to evaluate the prediction error
-#' @param weight Type of weight to use at the time of calculating the predicted value with a weighted mean. 
+#' @param weight Type of weight to use at the time of calculating the predicted value with a weighted mean.
 #- Three supported: proximity, same, trend.
 #' \describe{
 #'   \item{proximity}{the weight assigned to each neighbor is proportional to its distance}
 #'   \item{same}{all neighbors are assigned with the same weight}
-#'   \item{trend}{nearest neighbor is assigned with weight k, second closest neighbor with weight k-1, and so on until the 
+#'   \item{trend}{nearest neighbor is assigned with weight k, second closest neighbor with weight k-1, and so on until the
 #'                least nearest neighbor which is assigned with a weight of 1.}
 #' }
 #' @param threads Number of threads to be used when parallelizing
 #' @param file Name or id of the files where the distances matrixes are saved
 #' @return A matrix of errors, optimal K & D
 
-knn_optim_parallelf2 = function(x, k, d, v = 1, error_metric = "MAE", weight = "proximity", threads = 3, file, rows = 1){
+knn_optim_parallelf2 = function(x, k, d, v = 1, error_metric = "MAE", weight = "proximity", threads = 3, file, rows){
   require(parallelDist)
   require(forecast)
   require(foreach)
   require(doParallel)
   require(iterators)
-  
+
+  threads <- ifelse(threads == 0, parallel::detectCores() - 1, threads)
+
   # Choose the appropiate index of the accuracy result, depending on the error_metric
-  error_type = switch(error_metric,
+  error_type <- switch(error_metric,
                       ME = {1},
                       RMSE = {2},
                       MAE = {3},
                       MPE = {4},
                       MAPE = {5}
   )
-  
+
   # Sort k or d vector if they are unsorted
   if (is.unsorted(k)) {
-    k <- sort(k)
+      k <- sort(k)
   }
   if (is.unsorted(d)) {
-    d <- sort(d)
+      d <- sort(d)
   }
-  
+
   # Initialization of variables to be used
   y <- matrix(x, ncol = NCOL(x))
   n <- NROW(y)
   m <- NCOL(y)
   ks <- length(k)
   ds <- length(d)
-  
+
   # Once we have all distances matrixes we proceed to evaluate in parallel with a different combination
   # of d and row.
-  # For each of the combinations we order all the neighbors(elements) by proximity and evaluate with 
+  # For each of the combinations we order all the neighbors(elements) by proximity and evaluate with
   # all the posible values for k, taking each time the k-Nearest ones, to make k predictions.
   # Finally when we have all the predictions we calculate the error for each prediction and store them
   # in the variable of the foreach loop.
-  
+
   init <- floor(n * 0.7)
-  #clust <- makeCluster(parallel::detectCores()-1)
   clust <- makeCluster(threads)
   registerDoParallel(cl = clust)
-  
-  raw_preds <- foreach(act_row = 2:(n - init + 1)) %:% foreach(act_d = iter(d)) %do% {
-    # Obtain the distances matrix for the actual d
-    # TODO: eliminar las variables para evitar duplicación innecesaria de memoria. De momento se deja así para simplificar cambios
-    d_index <- match(act_d, d)
-    preds <- vector(mode = "numeric", ks)
-    
-    # Read from file
-    num_of_file <- ((act_row - 1) %/% rows) + 1
-    i <- 1
-    act_column_length <- n - act_row + ((act_row - 1) %% rows) - act_d + 1
-    j <- 1
-    row_in_file <- ((act_row - 1) %% rows) + 1
-    while (j < row_in_file) {
-      i = i + act_column_length
-      act_column_length =  act_column_length - 1
-      j = j + 1
-    }
-    
-    distances_element <- readRDS(paste0(paste0(file, act_d), num_of_file))[i:(i + n - act_row - 1 - act_d + 1)]
-    # For k = act_row get the indexes of all neighbors(elements) ordered by distance
-    dist_row <- sort.int(distances_element, index.return = TRUE)
-    
+
+  all_predictions <- foreach(i = 1:ds, .combine = cbind) %:% foreach(j = (n - init + 1):2, .combine = cbind) %dopar% {
+    predictions <- vector(mode = "numeric", ks)
+
+    # Get column needed from the distances matrix and sort it
+    num_of_file <- ((j - 1) %/% rows) + 1
+    distances_matrix_size <- n - d[i] - rows * (num_of_file - 1)
+    j_in_file <-((j - 1) %% rows + 1)
+    initial_index <- distances_matrix_size * (j_in_file - 1) - j_in_file * (j_in_file - 1) / 2 + 1
+    distances_col <- readRDS(paste0(file, d[i], "_", num_of_file))[initial_index:(initial_index + n - d[i] - j)]
+    sorted_distances_col <- sort.int(distances_col, index.return = TRUE)
+
     for (k_index in 1:ks) {
-      k_value <- k[k_index]
-      
-      # Get the indexes k nearest neighbors(elements)
-      k_nn <- head(dist_row$ix, k_value)
-      
-      # Calculate the weights for the future computation of the weighted mean
-      weights <- switch(weight, 
-                        proximity = {1/(distances_element[k_nn] + 1)},
-                        same = {rep.int(1,k_value)},
-                        trend = {k_value:1})
-      
-      # Calculate the predicted value
-      preds[k_index] <- weighted.mean(y[n - act_row + 2 - k_nn, v], weights)
+        k_value <- k[k_index]
+
+        # Get the indexes k nearest neighbors(elements)
+        k_nn <- head(sorted_distances_col$ix, k_value)
+
+        # Calculate the weights for the future computation of the weighted mean
+        weights <- switch(weight,
+                          proximity = {1 / (distances_col[k_nn] + .Machine$double.xmin * 1e150)},
+                          same = {rep.int(1, k_value)},
+                          trend = {k_value:1})
+
+        # Calculate the predicted value
+        predictions[k_index] <- weighted.mean(y[n - j + 2 - k_nn, v], weights)
     }
-    
-    list(d_index = d_index, instant_index = n - act_row - init + 2, preds = preds)
+
+    predictions
   }
-  
+
   registerDoSEQ()
   stopCluster(clust)
-  
-  # Change the format of raw_preds to d matrixes of k x (n - init) dimension in order
-  preds_list <- vector("list", ds)
-  j <- 1
-  for (i in d) {
-    preds_list[[j]] <- matrix(nrow = ks, ncol = n - init)
-    j <- j + 1
-  }
-  
-  for (l_elems in raw_preds) {
-    for (elem in l_elems){
-      preds_list[[elem$d_index]][, elem$instant_index] <- elem$preds
-    }
-  }
-  
+
   # Calculate error values between the known values and the predicted values, these values go from init to t - 1
   # and for all Ks
   errors <- matrix(nrow = ks, ncol = ds)
-  real_values <- y[(init + 1):n, v]
+  real_values <- matrix(y[(init + 1):n, v])
   for (i in 1:ds) {
-    for (ind in 1:ks) {
-      errors[ind, i] <- accuracy(ts(preds_list[[i]][ind, ]), matrix(real_values))[error_type]
-    }
+      initial_index <- (i - 1) * (n - init) + 1
+      for (k_index in 1:ks) {
+          errors[k_index, i] <- accuracy(ts(all_predictions[k_index, initial_index:(initial_index + n - init - 1)]), real_values)[error_type]
+      }
   }
-  
+
   # Construction of the list to be returned
   index_min_error <- which.min(errors)
   optK <- k[((index_min_error - 1) %% ks) + 1]
   optD <- d[ceiling(index_min_error / ks)]
   result <- list(errors = errors, k = optK, d = optD)
-  
-  result
+
+  return(result)
 }
