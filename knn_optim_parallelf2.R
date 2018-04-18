646d02c5ff06baba4489422590b25d5095be4308
javiberd <33233141+javiberd@users.noreply.github.com>
2018-04-18 03:16:44 +0200

    Much more efficient use of files, each thread now reads and uses all of the file

--- knn_optim_parallelf2.R
+++ knn_optim_parallelf2.R
@@ -17,7 +17,7 @@
 #' @param file Name or id of the files where the distances matrixes are saved
 #' @return A matrix of errors, optimal K & D
 
-knn_optim_parallelf2 = function(x, k, d, v = 1, init, error_metric = "MAE", weight = "proximity", threads = 3, file, rows){
+knn_optim_parallelf2 = function(x, k, d, v = 1, error_metric = "MAE", weight = "proximity", threads = 3, file, rows){
   require(parallelDist)
   require(forecast)
   require(foreach)
@@ -28,19 +28,19 @@ knn_optim_parallelf2 = function(x, k, d, v = 1, init, error_metric = "MAE", weig
 
   # Choose the appropiate index of the accuracy result, depending on the error_metric
   error_type <- switch(error_metric,
-                      ME = {1},
-                      RMSE = {2},
-                      MAE = {3},
-                      MPE = {4},
-                      MAPE = {5}
+                       ME = {1},
+                       RMSE = {2},
+                       MAE = {3},
+                       MPE = {4},
+                       MAPE = {5}
   )
 
   # Sort k or d vector if they are unsorted
   if (is.unsorted(k)) {
-      k <- sort(k)
+    k <- sort(k)
   }
   if (is.unsorted(d)) {
-      d <- sort(d)
+    d <- sort(d)
   }
 
   # Initialization of variables to be used
@@ -61,18 +61,38 @@ knn_optim_parallelf2 = function(x, k, d, v = 1, init, error_metric = "MAE", weig
   clust <- makeCluster(threads)
   registerDoParallel(cl = clust)
 
-  all_predictions <- foreach(i = 1:ds, .combine = cbind) %:% foreach(j = (n - init + 1):2, .combine = cbind) %dopar% {
-    predictions <- vector(mode = "numeric", ks)
+  if (rows == 1) {
+    num_of_file_array <- 2:(ceiling((n - init + 1) / rows))
+  }
+  else {
+    num_of_file_array <- 1:(ceiling((n - init + 1) / rows))
+  }
 
-    # Get column needed from the distances matrix and sort it
-    num_of_file <- ((j - 1) %/% rows) + 1
-    distances_matrix_size <- n - d[i] - rows * (num_of_file - 1)
-    j_in_file <-((j - 1) %% rows + 1)
-    initial_index <- distances_matrix_size * (j_in_file - 1) - j_in_file * (j_in_file - 1) / 2 + 1
-    distances_col <- readRDS(paste0(file, d[i], "_", num_of_file))[initial_index:(initial_index + n - d[i] - j)]
-    sorted_distances_col <- sort.int(distances_col, index.return = TRUE)
+  all_predictions <- foreach(i = 1:ds, .combine = cbind) %:% foreach(num_of_file = num_of_file_array, .combine = cbind) %dopar% {
+    num_cols_in_file <- ifelse(num_of_file * rows > n - init + 1, (n - init + 1) %% rows, rows)
 
-    for (k_index in 1:ks) {
+    if (num_of_file == 1 && rows > 1) {
+      predictions <- matrix(nrow = ks, ncol = num_cols_in_file - 1)
+      j_in_file_array <- 2:num_cols_in_file
+    }
+    else {
+      predictions <- matrix(nrow = ks, ncol = num_cols_in_file)
+      j_in_file_array <- 1:num_cols_in_file
+    }
+    # Get distances matrix
+    distances_matrix_size <- n - d[i] - rows * (num_of_file - 1) + 1
+    distances_matrix <- readRDS(paste0(file, d[i], "_", num_of_file))
+
+    # The ifelse its because the first column of the fist file is not use
+    for (j_in_file in j_in_file_array) {
+      # Get column and sort it
+      initial_index <- distances_matrix_size * (j_in_file - 1) - j_in_file * (j_in_file - 1) / 2 + 1
+      distances_col <- distances_matrix[initial_index:(initial_index + distances_matrix_size - j_in_file - 1)]
+      sorted_distances_col <- sort.int(distances_col, index.return = TRUE)
+
+      print(sorted_distances_col$x)
+
+      for (k_index in 1:ks) {
         k_value <- k[k_index]
 
         # Get the indexes k nearest neighbors(elements)
@@ -84,8 +104,19 @@ knn_optim_parallelf2 = function(x, k, d, v = 1, init, error_metric = "MAE", weig
                           same = {rep.int(1, k_value)},
                           trend = {k_value:1})
 
-        # Calculate the predicted value
-        predictions[k_index] <- weighted.mean(y[n - j + 2 - k_nn, v], weights)
+        # print(k_nn)
+        # print(y[n - (num_of_file - 1) * 3 - j_in_file + 2 - k_nn, v])
+        # print(weighted.mean(y[n - (num_of_file - 1) * rows - j_in_file + 2 - k_nn, v], weights))
+        #Calculate the predicted value
+        if (num_of_file == 1 && rows > 1) {
+          predictions[k_index, j_in_file - 1] <- weighted.mean(y[n - (num_of_file - 1) * rows - j_in_file + 2 - k_nn, v], weights)
+        }
+        else {
+          predictions[k_index, j_in_file] <- weighted.mean(y[n - (num_of_file - 1) * rows - j_in_file + 2 - k_nn, v], weights)
+
+        }
+        # print(predictions)
+      }
     }
 
     predictions
@@ -99,10 +130,10 @@ knn_optim_parallelf2 = function(x, k, d, v = 1, init, error_metric = "MAE", weig
   errors <- matrix(nrow = ks, ncol = ds)
   real_values <- matrix(y[(init + 1):n, v])
   for (i in 1:ds) {
-      initial_index <- (i - 1) * (n - init) + 1
-      for (k_index in 1:ks) {
-          errors[k_index, i] <- accuracy(ts(all_predictions[k_index, initial_index:(initial_index + n - init - 1)]), real_values)[error_type]
-      }
+    initial_index <- (i - 1) * (n - init) + 1
+    for (k_index in 1:ks) {
+      errors[k_index, i] <- accuracy(ts(all_predictions[k_index, initial_index:(initial_index + n - init - 1)]), rev(real_values))[error_type]
+    }
   }
 
   # Construction of the list to be returned
