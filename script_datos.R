ffb86b288e23e86fafd7fbdf6aa54289d8a01572
Dani-Basta <33234517+Dani-Basta@users.noreply.github.com>
2018-05-02 23:50:09 +0200

    Modified different plots
    - Contour: customized contour with max value, range for contour lines and quantity. Also added the best optimization values in the plot (still in development). 
    - Error bars: erased scalation of error with real values (posible problem if the series has very high values).

--- script_datos.R
+++ script_datos.R
@@ -13,7 +13,7 @@ train_init <- floor(n * 0.7)
 test_init <- floor(n * 0.9)
 x_train <- x[1:test_init]
 distance <- "euclidean"
-error_metric <-  "MAE"
+error_metric <-  "RMSE"
 weight <- "proximity"
 nThreads <- 7
 ks <-  1:50
@@ -23,21 +23,33 @@ min_x <- min(x)
 max_x <- max(x)
 
 # Get best k and d
-res <- knn_optim(x = x_train, k = ks, d = ds, init= train_init, distance_metric = distance, error_metric = error_metric, weight = weight)
+res <- knn_optim(x = x_train, k = ks, d = ds, init = train_init, distance_metric = distance, 
+                 error_metric = error_metric, weight = weight)
 
-euc_prox_train <- knn_past(x = x_train, k = res$k, d = res$d, init = train_init, distance_metric = distance, weight = "proximity", threads = nThreads)
-euc_prox_test <- knn_past(x = x, k = res$k, d = res$d, init = test_init, distance_metric = distance, weight = "proximity", threads = nThreads)
+euc_prox_train <- knn_past(x = x_train, k = res$k, d = res$d, init = train_init, 
+                           distance_metric = distance, weight = "proximity", threads = nThreads)
+euc_prox_test <- knn_past(x = x, k = res$k, d = res$d, init = test_init, 
+                          distance_metric = distance, weight = "proximity", threads = nThreads)
 euc_prox <- c(euc_prox_train, euc_prox_test)
 
 x_err <- ts(x[(train_init + 1):n])
 x_train_err <- ts(x[(train_init + 1):test_init])
 x_test_err <- ts(x[(test_init + 1):n])
 sub_dates <- tail(dates, length(x) - train_init)
-naive <- x[train_init:(NROW(x)-1)]
+naive <- x[train_init:(n - 1)]
+cont_min <- min(res$errors)
+cont_max_fix <- (max(res$errors) - (max(res$errors) - cont_min)*0.4)
+num_contours <- 40
+
+minimums <- head( sort.int(res$errors, index.return = TRUE)$ix , 5)
+
+x_minims <- minimums %% max(ks)
+y_minims <- ceiling(minimums/max(ks))
+
 # Exponential smoothing:
 # alpha = NULL - default alpha is estimated
 # initial = "simple" - the initial values are set to values obtained using simple calculations on the first few observations
-exp_smoothing <- fitted(ses(x, alpha = NULL, initial = "simple"))[(train_init+1):n]
+exp_smoothing <- fitted(ses(x, alpha = NULL, initial = "simple"))[(train_init + 1):n]
 
 # Data for errors table
 names_col <- c("Proximity")
--- script_graficas.R
+++ script_graficas.R
@@ -7,23 +7,44 @@ pMain <- plot_ly(x = dates, y = x, type = "scatter",  name = "Real", mode = "lin
 pMain <- add_trace(pMain, x = sub_dates, y = euc_prox, name = "Proximity")
 pMain <- add_trace(pMain, x = sub_dates, y = naive, name = "Naive") 
 pMain <- add_trace(pMain, x = sub_dates, y = exp_smoothing, name = "Exponential Smoothing")
-pMain <- add_segments(pMain, x = dates[train_init], xend = dates[train_init], y = min_x - 0.10 * (max_x - min_x), yend = max_x + 0.10 * (max_x - min_x), name = "Train")
-pMain <- add_segments(pMain, x = dates[test_init], xend = dates[test_init], y = min_x - 0.10 * (max_x - min_x), yend = max_x + 0.10 * (max_x - min_x), name = "Test")
+pMain <- add_segments(pMain, x = dates[train_init], xend = dates[train_init], y = min_x - 0.10 * (max_x - min_x), 
+                      yend = max_x + 0.10 * (max_x - min_x), name = "Train")
+pMain <- add_segments(pMain, x = dates[test_init], xend = dates[test_init], y = min_x - 0.10 * (max_x - min_x), 
+                      yend = max_x + 0.10 * (max_x - min_x), name = "Test")
 
 pMain <- layout(pMain, title = "Prediccion con distancia Euclídea", xaxis = list(rangeslider = list(type = "date")))
 
-# Error bars
-pBarsMain <- plot_ly(x = sub_dates, y = x_err - euc_prox, name = "Proximity error", type = "bar")
-pBarsMain <- add_trace(pBarsMain, x = sub_dates, y = x_err - naive, name = "Naive error") 
-pBarsMain <- add_trace(pBarsMain, x = sub_dates, y = x_err - exp_smoothing, name = "Exponential Smoothing error") 
-pBarsMain <- layout(pBarsMain, yaxis = list( range = c( max(x_err)*-1, max(x_err) ) ))
-combPlotMain <- subplot(pMain, pBarsMain, nrows = 2, shareX = TRUE)
+#                       Normal errors
+#If we have more than 1 error to show is more legible if we use a line plot instead of a bar one
+#pErrMain <- 
+    #plot_ly(x = sub_dates, y = x_err - euc_prox, name = "Proximity error", type = "bar")
+    #plot_ly(x = sub_dates, y = x_err - euc_prox, name = "Proximity error", type = "scatter", mode = "lines")
+#pErrMain <- add_trace(pErrMain, x = sub_dates, y = x_err - naive, name = "Naive error") 
+#pErrMain <- add_trace(pErrMain, x = sub_dates, y = x_err - exp_smoothing, name = "Exponential Smoothing error")
+#In series with very high values and low errors, graphic can be nearly invisible if scalated to the series
+#pErrMain <- layout(pErrMain, yaxis = list( range = c( max(x_err)*-1, max(x_err) ) ))
+
+
+#                       Absolute errors
+#If we have more than 1 error to show is more legible if we use a line plot instead of a bar one
+pErrMain <- 
+    #plot_ly(x = sub_dates, y = x_err - euc_prox, name = "Proximity error", type = "bar")
+    pErrMain <- plot_ly(x = sub_dates, y = abs(x_err - euc_prox), name = "Proximity error", type = "scatter", mode = "lines")
+
+pErrMain <- add_trace(pErrMain, x = sub_dates, y = abs(x_err - naive), name = "Naive error") 
+pErrMain <- add_trace(pErrMain, x = sub_dates, y = abs(x_err - exp_smoothing), name = "Exponential Smoothing error")
+
+
+combPlotMain <- subplot(pMain, pErrMain, nrows = 2, shareX = TRUE)
+
 
 #                               Plots for optimization tab
 pOpt <- plot_ly(x = dates, y = x, type = "scatter",  name = "Real", mode = "lines")
 pOpt <- add_trace(pOpt, x = sub_dates, y = euc_prox, name = "Proximity")
-pOpt <- add_segments(pOpt, x = dates[test_init], xend = dates[test_init], y = min_x - 0.10 * (max_x - min_x), yend = max_x + 0.10 * (max_x - min_x), name = "Train")
-pOpt <- add_segments(pOpt, x = dates[test_init], xend = dates[test_init], y = min_x - 0.10 * (max_x - min_x), yend = max_x + 0.10 * (max_x - min_x), name = "Test")
+pOpt <- add_segments(pOpt, x = dates[train_init], xend = dates[train_init], y = min_x - 0.10 * (max_x - min_x), 
+                     yend = max_x + 0.10 * (max_x - min_x), name = "Train")
+pOpt <- add_segments(pOpt, x = dates[test_init], xend = dates[test_init], y = min_x - 0.10 * (max_x - min_x), 
+                     yend = max_x + 0.10 * (max_x - min_x), name = "Test")
 
 pOpt <- layout(pOpt, title = "Prediccion con distancia Euclídea", xaxis = list(rangeslider = list(type = "date")))
 
@@ -32,6 +53,14 @@ pBarsOpt <- plot_ly(x = sub_dates, y = x_err - euc_prox, name = "Prediction erro
 
 combPlotOpt <- subplot(pOpt, pBarsOpt, nrows = 2, shareX = TRUE)
 
-pContour <- plot_ly(x = ks , y = ds, z = t(res$errors), type = "contour", autocontour = TRUE, contours = list(showlabels = TRUE, coloring = "heatmap"))
+pContour <- plot_ly(x = ks , y = ds, z = res$errors, transpose = TRUE, type = "contour", 
+                    #Just indicate the number of line contours to plot
+                    #autocontour = TRUE, contours = list(showlabels = TRUE, coloring = "heatmap") , ncontours = num_contours ,
+                    
+                    #version saying where to start doing lines, space between lines and where to stop
+                    contours = list(showlabels = TRUE, coloring = "heatmap", start = cont_min, end = (cont_max_fix+cont_min)/2, size = (((cont_max_fix+cont_min)/2)-cont_min)/num_contours),
+                    
+                    zmin = cont_min, zmax = cont_max_fix)
 pContour <- layout(pContour, title = "MAE Optimization", xaxis = list(title = "K"), yaxis = list(title = "D") )
-
+###provisional
+#add_trace(pContour, type = "scatter", mode = "markers",  x = x_minims, y = y_minims, marker = list(color = "red"), showlegend = FALSE)
