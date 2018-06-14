7944603d04f888d5d2938d18c0db378c9146f305
javiberd <33233141+javiberd@users.noreply.github.com>
2018-06-15 01:11:59 +0200

    Examples and format change

--- knn_elements.R
+++ knn_elements.R
@@ -6,19 +6,22 @@
 #' @param y A matrix.
 #' @param d Length of each of the 'elements'.
 #' @return A matrix to be used for calculating distances.
+#' @examples
+#' knn_elements(matrix(AirPassengers), 2)
+#' knn_elements(matrix(LakeHuron), 6)
 
-knn_elements = function(y, d) {
+knn_elements <- function(y, d) {
   n <- NROW(y)
   m <- NCOL(y)
   last_elem <- n - d
 
   # Fill matrix as described above, it is done vertically for efficiency reasons
   elements_matrix <- matrix(nrow = last_elem + 1, ncol = d * m)
-  col <- NCOL(elements_matrix)
+  col <- 1
   for (i in 1:m) {
     for (j in 1:d) {
       elements_matrix[, col] <- rev(y[(j:(j + last_elem)), i])
-      col <- col - 1
+      col <- col + 1
     }
   }
 
