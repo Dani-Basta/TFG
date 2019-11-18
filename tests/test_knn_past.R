231654e3e99b6a473ee2495ebc43fda26bf0bd3d
javiberd <javierberdeciot@gmail.com>
2019-11-18 21:08:51 +0100

    Added tests.

--- tests/test_knn_past.R
+++ tests/test_knn_past.R
@@ -1,32 +1,32 @@
-source("../knn_next.R", chdir = TRUE)
+source("../knn_past.R", chdir = TRUE)
 require(testthat)
 
-test_data_dir = "data/knn_next"
+test_data_dir = "data/knn_past"
 expected_res_file_path_1 = paste0(test_data_dir, "/expected_res_1") 
 expected_res_file_path_2 = paste0(test_data_dir, "/expected_res_2") 
 expected_res_file_path_3 = paste0(test_data_dir, "/expected_res_3") 
 expected_res_file_path_4 = paste0(test_data_dir, "/expected_res_4") 
 
-test_that("knn_next() for a univariate time series (length: 98)", {
-	res = knn_next(LakeHuron, 3, 6)
+test_that("knn_past() for a univariate time series (length: 98)", {
+	res = knn_past(LakeHuron, 5, 5)
 	expected_res = readRDS(expected_res_file_path_1) 
 	expect_equal(res, expected_res)
 })
 
-test_that("knn_next() for a multivariate time series (length: 72)", {
-	res = knn_next(matrix(AirPassengers, ncol = 2), 5, 2)
+test_that("knn_past() for a multivariate time series (length: 72)", {
+	res = knn_past(matrix(AirPassengers, ncol = 2), 4, 3)
 	expected_res = readRDS(expected_res_file_path_2) 
 	expect_equal(res, expected_res)
 }) 
 
-test_that("knn_next() for a univariate time series (length: 3177)", {
-	res = knn_next(sunspot.month, 15, 12) 
+test_that("knn_past() for a univariate time series (length: 3177)", {
+	res = knn_past(sunspot.month, 14, 10) 
 	expected_res = readRDS(expected_res_file_path_3) 
 	expect_equal(res, expected_res)
 }) 
 
-test_that("knn_next() for a multivariate time series (length: 1059)", {
-	res = knn_next(matrix(sunspot.month, ncol = 3), 5, 6)
+test_that("knn_past() for a multivariate time series (length: 1059)", {
+	res = knn_past(matrix(sunspot.month, ncol = 3), 6, 8)
 	expected_res = readRDS(expected_res_file_path_4) 
 	expect_equal(res, expected_res)
 })
